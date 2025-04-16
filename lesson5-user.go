e2d97a513627fe614d1c676abfddeee44b13bd4f
Anastasiia <lapochkanep@gmail.com>
2025-04-16 17:00:10 +0300

    Add files via upload

---
+++ lesson5-user.go
@@ -0,0 +1,33 @@
+package users
+
+import (
+	"errors"
+)
+
+var (
+	ErrUserNotFound = errors.New("user not found")
+)
+
+type User struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type Service struct {
+}
+
+func (s *Service) CreateUser( /* Create user params */) (*User, error) {
+
+}
+
+func (s *Service) ListUsers() ([]User, error) {
+	// ...
+}
+
+func (s *Service) GetUser(userID string) (*User, error) {
+	// ...
+}
+
+func (s *Service) DeleteUser(userID string) error {
+	// ...
+}
